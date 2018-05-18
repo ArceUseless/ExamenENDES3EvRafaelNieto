e384f9631e4d203e85f7bcf3495fb13d63787d9c
rafa245 <arceuse1999@gmail.com>
2018-05-18 14:21:19 +0200

    ExamenEndesRafaelNieto

---
+++ TestCCuenta.java
@@ -0,0 +1,77 @@
+package examenEntornos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TestCCuenta {
+	private static BufferedReader dato;
+
+	public static void main(String[] args) {
+		dato = new BufferedReader(new InputStreamReader(
+				System.in));
+		CCuenta RafaelNieto;
+
+		int opcion = 0;
+		RafaelNieto = new CCuenta("Rigoberta Piedra", "0000-6523-85-678912345",
+				2500, 0);
+		do {
+			try {
+				mostrarMenu();
+				opcion = recogerOpcion(dato);
+	
+				switch (opcion) {
+				case 1:
+					ingresar(dato, RafaelNieto);
+					break;
+				case 2:
+					retirar(dato, RafaelNieto);
+					break;
+				case 3:
+					System.out.println("Aaaaaaaaaadios");
+				}
+			} catch (IOException ex) {
+				Logger.getLogger(TestCCuenta.class.getName()).log(Level.SEVERE,
+						null, ex);
+			}
+		} while (opcion != 3);
+		System.out.println("Saldo actual: " + RafaelNieto.estado());
+	}
+
+	private static void retirar(BufferedReader dato, CCuenta cuenta1) throws IOException {
+		System.out.println("Indica cantidad a retirar: ");
+		float retirar = Integer.parseInt(dato.readLine());
+		try {
+			cuenta1.retirar(retirar);
+		} catch (Exception e) {
+			System.out.print("Fallo al retirar");
+		}
+	}
+
+	private static void ingresar(BufferedReader dato, CCuenta cuenta1) throws IOException {
+		System.out.println("Indica cantidad a ingresar: ");
+		float ingresar = Integer.parseInt(dato.readLine());
+		try {
+			System.out.println("Ingreso en cuenta");
+			cuenta1.ingresando(ingresar);
+		} catch (Exception e) {
+			System.out.print("Fallo al ingresar");
+		}
+	}
+
+	private static int recogerOpcion(BufferedReader dato) throws IOException {
+		int opcion;
+		opcion = Integer.parseInt(dato.readLine());
+		return opcion;
+	}
+
+	private static void mostrarMenu() {
+		System.out.println("MENU DE OPERACIONES");
+		System.out.println("-------------------");
+		System.out.println("1 - Ingresar");
+		System.out.println("2 - Retirar");
+		System.out.println("3 - Salir");
+	}
+}
